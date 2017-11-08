0de248b01f9eda037d74cb0d42822e9def5eba08
Mahmood Zamani <imahmoodzamani@gmail.com>
2017-11-08 12:14:37 +0330

    add signIn

---
+++ src/screens/loginScreen.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import {Icon, Text} from 'native-base';
+
+class ProfileScreen extends Component {
+  static navigationOptions = {
+    tabBarLabel: 'پروفایل',
+    title: 'پروفایل',
+    headerTitleStyle: {
+      fontSize: 19,
+      fontFamily: 'IRANSansMobile',
+      fontWeight: '400'
+    },
+    tabBarIcon: ({tintColor}) => (
+      <Icon name="person" style={{color: tintColor}}/>
+    ),
+  };
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <View style={styles.menuItem}>
+          <TouchableOpacity onPress={() => console.log ('sd')}>
+            <Text style={styles.loginButton}>وارد شوید</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create ({
+  container: {
+    alignItems: 'flex-start'
+  },
+  menuItem: {
+    alignSelf: 'stretch',
+    borderBottomColor: '#ababab',
+    borderBottomWidth: 1
+  },
+  loginButton: {
+    fontFamily: "IRANSansMobile",
+    padding: 10,
+    textAlign: 'left'
+  }
+});
+
+export default ProfileScreen;
